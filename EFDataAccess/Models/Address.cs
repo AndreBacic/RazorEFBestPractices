10c9c7ea325b90fef35e5ee1e36f13c7b1664305
andre9050@yahoo.com <andre9050@yahoo.com>
2022-04-13 23:28:38 -0400

    Add project files.

---
+++ EFDataAccess/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace EFDataAccess.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; } // USA is assumed
+        public string ZipCode { get; set; }
+    }
+}
---
+++ EFDataAccess/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccess.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Priority { get; set; }
+    }
+}
